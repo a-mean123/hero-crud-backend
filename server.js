c42dfdb6f0c093e99d63e815776292067e6fd751
a-mean123 <mohamedamine.jebali@esen.tn>
2022-05-05 02:56:01 +0100

    last commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const mongoose = require('./config/connect.js');
 
 const heroRoute = require('./routes/hero.js');
 
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 //http://127.0.0.1:3000
 
 app.use( '/hero' , heroRoute );
